b9120a60b239055df3097d06055bc82b260ae75e
perakasem <pkasemsripitak@gmail.com>
2023-08-28 12:35:19 +0700

    database creation

---
+++ db_init.py
@@ -0,0 +1,47 @@
+import sqlite3
+from os.path import join, dirname, abspath
+import sys
+import csv
+
+def create_db(name):
+    db_path = str(join(dirname(dirname(abspath(__file__))), f'mathletics/comp_dbs/{name}.db'))
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+
+    # create a table for questions
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS questions (
+            id INTEGER PRIMARY KEY,
+            answer TEXT,
+            base_score INTEGER
+        )
+    ''')
+
+    # create a table for question logging
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS progress (
+            qid INTEGER,
+            tid INTEGER,
+            attempts INTEGER,
+            time INTEGER,
+            completed integer DEFAULT 0
+        )
+    ''')
+
+    # create a table for teams
+    # members and completed_questions stores lists as JSON strings
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS teams (
+            id INTEGER PRIMARY KEY,
+            team_name TEXT,
+            members TEXT,
+            completed_qid TEXT,
+            score INTEGER
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+create_db("test")
+    
